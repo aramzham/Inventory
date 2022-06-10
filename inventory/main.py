31943f72ba7a2f3be5058f9e4c792699b7c9e1be
azhamkochyan <aram.zhamkochyan@dataart.com>
2022-06-10 15:29:32 +0200

    initial commit

---
+++ inventory/main.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host="redis-10757.c55.eu-central-1-1.ec2.cloud.redislabs.com",
+    port=10757,
+    password="vPCbrzwqkRNWTkafnQTb57kjP3dbLODG",
+    decode_responses=True
+)
+
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id':product.pk,
+        'name':product.name,
+        'price':product.price,
+        'quantity':product.quantity
+    }
+
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
+
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+
+@app.get("/products")
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
